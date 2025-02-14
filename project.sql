d07516747a12c613fb8a723b6f8597ecc7831ed2
divishajain12-pj <divishajain1210@gmail.com>
2025-02-14 22:11:34 +0530

    act2

---
+++ project.sql
@@ -0,0 +1,31 @@
+create TABLE salesman (
+ID int,
+name varchar (255),
+age varchar (255),
+city varchar (255),
+salary varchar(255)
+)
+
+INSERT into salesman(
+id,name,age,city,salary)
+VALUES(
+1,'Arun',35,'jaipur',25000)
+INSERT INTO salesman(
+id,name,age,city,salary)
+VALUES(
+2,'Aruna',39,'delhi',35000)
+INSERT INTO salesman(
+id,name,age,city,salary)
+VALUES(
+3,'Arica',23,'mumbai',45000)
+INSERT INTO salesman(
+id,name,age,city,salary)
+VALUES(
+4,'rahul',43,'pune',36000)
+SELECT * FROM salesman
+
+
+SELECT * from salesman
+WHERE name= 'Aruna'
+
+
